da8bf9bd73c891822400460f1bc5cc83116587e7
@babinm1h <babinmih@mail.ru>
2022-12-09 07:24:06 +0300

    verc

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const passport = require("passport");
 const app_module_1 = require("./app.module");
-const session = require("express-session");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { bodyParser: true });
     const PORT = process.env.PORT || 7777;
@@ -14,11 +13,6 @@ async function bootstrap() {
         ],
         credentials: true,
     });
-    app.use(session({
-        secret: process.env.SESSION_SECRET,
-        saveUninitialized: false,
-        resave: false,
-    }));
     app.use(passport.initialize());
     app.use(passport.session());
     await app.listen(PORT, () => console.log(`${PORT} started`));
--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,7 @@ async function bootstrap() {
     ],
     credentials: true,
   });
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET || 'r4nd0m',
-      saveUninitialized: false,
-      resave: false,
-    }),
-  );
+
   app.use(passport.initialize());
   app.use(passport.session());
   await app.listen(PORT, () => console.log(`${PORT} started`));
